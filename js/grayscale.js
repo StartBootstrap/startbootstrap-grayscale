e939af458a0c6dae7d333eb747bf37f2cf1b1970
DAVID HALLINAN <davidhallinan@DAVIDs-MacBook-Pro.local>
2015-07-20 16:16:23 -0700

    update site a lot

--- js/grayscale.js
+++ js/grayscale.js
@@ -38,25 +38,25 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+        zoom: 14,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(37.909203, -122.312388), // Richmond
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
         scrollwheel: false,
         draggable: false,
 
-        // How you would like to style the map. 
+        // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             "featureType": "water",
             "elementType": "geometry",
             "stylers": [{
-                "color": "#000000"
+                "color": "#1F2E4C"
             }, {
-                "lightness": 17
+                "lightness": 20
             }]
         }, {
             "featureType": "landscape",
@@ -70,9 +70,9 @@ function init() {
             "featureType": "road.highway",
             "elementType": "geometry.fill",
             "stylers": [{
-                "color": "#000000"
+                "color": "#1A1400"
             }, {
-                "lightness": 17
+                "lightness": 25
             }]
         }, {
             "featureType": "road.highway",
@@ -98,7 +98,7 @@ function init() {
             "stylers": [{
                 "color": "#000000"
             }, {
-                "lightness": 16
+                "lightness": 25
             }]
         }, {
             "featureType": "poi",
@@ -160,7 +160,7 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
@@ -169,7 +169,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(37.909203, -122.312388);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
