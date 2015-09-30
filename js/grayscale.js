8d8f62f264adcafe6caf37e60b9d3e8033318b46
David M. Zendzian @ rtwap <dmz@zzservers.com>
2015-09-30 14:33:54 -0400

    Updated grayscale.js to support dropdown menus on mobile displays
    Signed-off-by: David M. Zendzian

--- js/grayscale.js
+++ js/grayscale.js
@@ -26,7 +26,9 @@ $(function() {
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
+  if ($(this).attr('class') != 'dropdown-toggle active') {
     $('.navbar-toggle:visible').click();
+  }
 });
 
 // Google Maps Scripts
