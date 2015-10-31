1ce43b996126eab2a35ad0026fa4181a8dd0810e
Jack Lamberti <jal416@lehigh.edu>
2015-10-31 07:56:01 -0400

    Fix for collapsing navbar on a page reload

--- js/grayscale.js
+++ js/grayscale.js
@@ -5,13 +5,16 @@
  */
 
 // jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
+function collapseNavbar() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-});
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
