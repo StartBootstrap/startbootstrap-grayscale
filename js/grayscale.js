121aff5d5d6d43217ef2496cbfeb6537b9af29a6
David Miller <hello@davidmiller.io>
2016-08-19 16:11:42 -0400

    gulpfile cleanup and dropdown toggle bugfix

--- js/grayscale.js
+++ js/grayscale.js
@@ -29,9 +29,7 @@ $(function() {
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
     $('.navbar-toggle:visible').click();
-  }
 });
 
 // Google Maps Scripts
