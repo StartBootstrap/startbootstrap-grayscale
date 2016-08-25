1475c14e46327a329bdf211cd1cc987d7cb93fdb
Mateus Vitali <mateus.c.vitali@gmail.com>
2016-08-25 10:28:01 -0300

    Fix Flashes when click on menu item (Closes the Responsive Menu on Menu Item Click)

--- js/grayscale.js
+++ js/grayscale.js
@@ -29,7 +29,7 @@ $(function() {
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-    $(this).closest('.collapse').collapse('toggle');
+    $(".navbar-collapse").collapse('hide');
 });
 
 // Google Maps Scripts
@@ -55,7 +55,7 @@ function init() {
         scrollwheel: false,
         draggable: false,
 
-        // How you would like to style the map. 
+        // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             "featureType": "water",
@@ -167,7 +167,7 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
