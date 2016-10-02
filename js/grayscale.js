2a176462e33f90644a4a4d313427172c2d01a6df
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2016-10-03 00:36:33 +0800

    Fixed blinking navbar when clicked

--- js/grayscale.js
+++ js/grayscale.js
@@ -28,8 +28,8 @@ $(function() {
 });
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $(this).closest('.collapse').collapse('toggle');
+$('.navbar-collapse ul li a').click(function(){ 
+    $('.navbar-toggle:visible').click();
 });
 
 // Google Maps Scripts
