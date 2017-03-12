1e65c26276f92a0cdd3cc783d7e19c7c4fcb96dd
scottbock <l.scott.bock@gmail.com>
2017-03-11 22:02:53 -0600

    update map info

--- js/grayscale.js
+++ js/grayscale.js
@@ -37,7 +37,7 @@ var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+    map.setCenter(new google.maps.LatLng(41.14587, -96.22635));
 });
 
 function init() {
@@ -48,7 +48,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(41.14587, -96.22635), // New York
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -176,7 +176,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(41.14587, -96.22635);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
