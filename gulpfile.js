abbdd9c6960a53fa906ef6221c6063117d3adb67
Ján Dugovič <mr.dugi@gmail.com>
2018-05-11 22:11:27 +0200

    minify html

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
-var browserSync = require('browser-sync').create();
+var htmlmin = require('gulp-htmlmin');
+var browserSync = require('browser-sync')
+  .create();
 
 // Set the banner content
 var banner = ['/*!\n',
@@ -17,7 +19,7 @@ var banner = ['/*!\n',
 ].join('');
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
+gulp.task('vendor', function () {
 
   // Bootstrap
   gulp.src([
@@ -53,7 +55,7 @@ gulp.task('vendor', function() {
 });
 
 // Compile SCSS
-gulp.task('css:compile', function() {
+gulp.task('css:compile', function () {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass.sync({
       outputStyle: 'expanded'
@@ -62,7 +64,7 @@ gulp.task('css:compile', function() {
 });
 
 // Minify CSS
-gulp.task('css:minify', ['css:compile'], function() {
+gulp.task('css:minify', ['css:compile'], function () {
   return gulp.src([
       './css/*.css',
       '!./css/*.min.css'
@@ -79,7 +81,7 @@ gulp.task('css:minify', ['css:compile'], function() {
 gulp.task('css', ['css:compile', 'css:minify']);
 
 // Minify JavaScript
-gulp.task('js:minify', function() {
+gulp.task('js:minify', function () {
   return gulp.src([
       './js/*.js',
       '!./js/*.min.js'
@@ -95,11 +97,24 @@ gulp.task('js:minify', function() {
 // JS
 gulp.task('js', ['js:minify']);
 
+//Minify HTML
+gulp.task('html:minify', function () {
+  return gulp.src('./index.dev.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(rename(('./index.html')))
+    .pipe(gulp.dest('.'));
+});
+
+//HTML
+gulp.task('html', ['html:minify']);
+
 // Default task
-gulp.task('default', ['css', 'js', 'vendor']);
+gulp.task('default', ['css', 'js', 'html', 'vendor']);
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
   browserSync.init({
     server: {
       baseDir: "./"
@@ -108,8 +123,9 @@ gulp.task('browserSync', function() {
 });
 
 // Dev task
-gulp.task('dev', ['css', 'js', 'browserSync'], function() {
+gulp.task('dev', ['css', 'js', 'html', 'browserSync'], function () {
   gulp.watch('./scss/*.scss', ['css']);
   gulp.watch('./js/*.js', ['js']);
-  gulp.watch('./*.html', browserSync.reload);
+  gulp.watch('./index.dev.html', ['html']);
+  gulp.watch('./index.dev.html', browserSync.reload);
 });
