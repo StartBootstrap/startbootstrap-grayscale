8d7a25202e38136ef27f2a66c70c1a40622fc33f
Joseph Lee <josephl@crista.net>
2019-03-31 15:21:25 -0700

    Fixed form display settings.

--- js/grayscale.js
+++ js/grayscale.js
@@ -40,3 +40,5 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
+
+(function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){js = ce.call(d, "script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()
