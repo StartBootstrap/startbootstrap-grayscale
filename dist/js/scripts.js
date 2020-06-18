cef8ee027b40d3dc8a45bf512a9aeba05222f313
David Miller <hello@davidmiller.io>
2020-06-18 17:04:28 -0400

    update dependencies, render pug script, and migration content

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
     * Start Bootstrap - Grayscale v6.0.1 (https://startbootstrap.com/themes/grayscale)
     * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-grayscale/blob/master/LICENSE)
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
     */
     (function ($) {
     "use strict"; // Start of use strict
