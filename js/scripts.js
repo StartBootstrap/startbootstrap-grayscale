bd53b60e09f403fd7d97e6da0fe5ab63853d316e
David Miller <hello@davidmiller.io>
2021-06-28 15:55:03 -0400

    Bootstrap 5.0.2 update and SB Forms integration

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Grayscale v7.0.1 (https://startbootstrap.com/theme/grayscale)
+* Start Bootstrap - Grayscale v7.0.2 (https://startbootstrap.com/theme/grayscale)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
 */
