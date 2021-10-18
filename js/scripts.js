afb4c01213a6ea3135a1c01a33cc9588d438091e
David Miller <hello@davidmiller.io>
2021-10-18 18:57:19 -0400

    Bootstrap 5.1.3 dependency update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Grayscale v7.0.3 (https://startbootstrap.com/theme/grayscale)
+* Start Bootstrap - Grayscale v7.0.4 (https://startbootstrap.com/theme/grayscale)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
 */
